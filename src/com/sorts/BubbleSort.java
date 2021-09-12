7bce54cb0e02e50531546f10be6fe0110ea84ac8
khodosyaroslav <khodos.jar@gmail.com>
2021-09-12 18:44:44 +0300

    more efficient solution

--- src/com/sorts/BubbleSort.java
+++ src/com/sorts/BubbleSort.java
@@ -1,5 +1,7 @@
 package com.sorts;
 
+import java.util.Arrays;
+
 public class BubbleSort {
 
 	public static void bubSort(int arr[]) {
@@ -25,36 +27,23 @@ public class BubbleSort {
 		}
 	}
 	
-	public static void show(int arr[]) {
-		for(int i = 0; i < arr.length; i++) {
-			System.out.print(arr[i] + " ");
-		}
-		System.out.println();
-	}
-	public static void show(String arr[]) {
-		for(int i = 0; i < arr.length; i++) {
-			System.out.print(arr[i] + " ");
-		}
-		System.out.println();
-	}
-	
 	public static void main(String[] args) {
 		int a[] = {64, 34, 25, 12, 22, 11, 90};
 		String astr[] = {"Rick", "Steve", "Ajeet", "Becky", "Mohan"};
 		
 		System.out.println("array of ints: ");
-		show(a);
+		System.out.println(Arrays.toString(a));
 		System.out.println("sorted array of ints: ");
-		bubSort(a);
-		show(a);
+		bubSort(a);;
+		System.out.println(Arrays.toString(a));
 		
 		System.out.println("----------------------");
 		
 		System.out.println("array of strings: ");
-		show(astr);
+		System.out.println(Arrays.toString(astr));
 		System.out.println("sorted array of strings: ");
 		bubSort(astr);
-		show(astr);
+		System.out.println(Arrays.toString(astr));
 		
 	}
 
