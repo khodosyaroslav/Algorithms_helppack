882a1d5759bd31c009411a45279f5691577db383
khodosyaroslav <khodos.jar@gmail.com>
2021-09-18 13:01:57 +0300

    added new datastucture

--- src/com/datastructs/linkedlist/LinkedList.java
+++ src/com/datastructs/linkedlist/LinkedList.java
@@ -1,9 +1,7 @@
 package com.datastructs.linkedlist;
 
-// TODO: delete method and reverse method
-
 public class LinkedList {
-	static ListItem head;
+	ListItem head;
 	
 	public void add(int data) {
 		ListItem newItem = new ListItem(data);
@@ -20,6 +18,42 @@ public class LinkedList {
 		}		
 	}
 	
+	public static LinkedList delete(LinkedList list, int item) {
+		ListItem current = list.head;
+		ListItem prev = null;
+		
+		if(current != null && current.data == item) {
+			list.head = current.next;
+			return list;
+		}
+		
+		while(current != null && current.data != item) {
+			prev = current;
+			current = current.next;
+		}		
+		if(current != null) {
+			prev.next = current.next;
+		}
+		
+		if(current == null)
+			System.out.println(item + " not found");
+		
+		return list;
+	}
+	
+	public void reverse() {
+		if(head != null && head.next != null) {
+			ListItem current = head.next;
+			head.next = null;
+			while(current != null) {
+				ListItem next = current.next;
+				current.next = head;
+				head = current;
+				current = next;
+			}
+		}
+	}
+	
 	public void print() {
 		ListItem current = head;
 		
@@ -32,15 +66,23 @@ public class LinkedList {
 	public static void main(String args[]) {
 		LinkedList linkList = new LinkedList();
 		
-		linkList.add(3);
-		linkList.add(4);
-		linkList.add(5);
-		linkList.add(6);
-		linkList.add(7);
-		linkList.add(8);
-		linkList.add(9);
-		linkList.add(9);
+		linkList.add(30);
+		linkList.add(40);
+		linkList.add(50);
+		linkList.add(60);
+		linkList.add(70);
+		linkList.add(80);
+		linkList.add(90);
+		linkList.add(91);
 
+		linkList.print();
+		System.out.println("\n------------------");
+		
+		linkList = delete(linkList, 60);		
+		linkList.print();
+		System.out.println("\n------------------");
+		
+		linkList.reverse();
 		linkList.print();
 	}
 	
